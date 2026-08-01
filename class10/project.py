66785c9c13d68674e32d223cbbfaaec91629484e
sathwikammu <sathwikammu2@gmail.com>
2026-08-01 21:49:52 +0530

    hi

---
+++ class10/project.py
@@ -0,0 +1,9 @@
+base=int(input("enter the base number:"))
+power=int(input("enter the power number:"))
+result=1
+for i in range(power):
+    result=result*base
+
+print("result=",result)
+
+
---
+++ test/project.py
@@ -0,0 +1,8 @@
+num=int(input("enter the number"))
+secret=int(45)
+if num==secret:
+    print("you won the game")
+else:
+    print("you loose the game")
+while 
+
